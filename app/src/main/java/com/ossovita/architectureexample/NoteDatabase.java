24c515ec84b5ceca21c559bceb8ebd1c6da56ea9
sancaryum <yasineryigit737@gmail.com>
2021-03-18 03:26:11 +0300

    Repository added

--- app/src/main/java/com/ossovita/architectureexample/NoteDatabase.java
+++ app/src/main/java/com/ossovita/architectureexample/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.ossovita.architectureexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class},version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -15,15 +18,37 @@ public abstract class NoteDatabase extends RoomDatabase {
 
     //Singleton yapısında senkronize bir şekilde database objesine ulaşılacak.
     public static synchronized NoteDatabase getInstance(Context context){
-        if(instance==null){//instance yoksa
+        if(instance==null) {//instance yoksa
             instance = Room.databaseBuilder(context.getApplicationContext()
-                    ,NoteDatabase.class
-                    ,"note_database")
+                    , NoteDatabase.class
+                    , "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
-            return instance;
-        }else{//instance varsa
-            return instance; //mevcut instance'i döndür
+        }
+        return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void>{
+        private NoteDao noteDao;
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao=db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1","Description1",1));
+            noteDao.insert(new Note("Title2","Description2",2));
+            noteDao.insert(new Note("Title3","Description3",3));
+            return null;
         }
     }
 }
