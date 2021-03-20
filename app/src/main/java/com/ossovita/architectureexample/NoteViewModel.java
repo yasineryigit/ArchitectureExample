e3cc84404d259f0f3093a077b6eb6c6ef5b5dbf0
yasineryigit <yasineryigit737@gmail.com>
2021-03-20 23:46:21 +0300

    NoteViewModel class added

---
+++ app/src/main/java/com/ossovita/architectureexample/NoteViewModel.java
@@ -0,0 +1,13 @@
+package com.ossovita.architectureexample;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
