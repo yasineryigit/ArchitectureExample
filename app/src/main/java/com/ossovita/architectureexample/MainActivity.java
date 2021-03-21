1ac9fc725ae16ac2d268fc2e2896ef559ff9fecd
yasineryigit <yasineryigit737@gmail.com>
2021-03-21 19:35:47 +0300

    RecyclerView Adapter added

--- app/src/main/java/com/ossovita/architectureexample/MainActivity.java
+++ app/src/main/java/com/ossovita/architectureexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.ossovita.architectureexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,12 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                //veriler değiştiği zaman adapter üzerinden setNotes metodu aracılığıyla güncel listeyi recycler'a gönderiyoruz
+                //en güncel notes listesini alıp recycler'a gönderiyoruz olay bu.
+                adapter.setNotes(notes);
             }
         });
     }
---
+++ app/src/main/java/com/ossovita/architectureexample/NoteAdapter.java
@@ -0,0 +1,60 @@
+package com.ossovita.architectureexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes = new ArrayList<>();
+
+    public void setNotes(List notes){
+        this.notes=notes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,parent,false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        holder.textViewTitle.setText(notes.get(position).getTitle());
+        holder.textViewDescription.setText(notes.get(position).getDescription());
+        holder.textViewPriority.setText(String.valueOf(notes.get(position).getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+
+
+    public class NoteHolder extends RecyclerView.ViewHolder{
+        private TextView textViewTitle;
+        private TextView textViewDescription;
+        private TextView textViewPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle=itemView.findViewById(R.id.text_view_title);
+            textViewPriority=itemView.findViewById(R.id.text_view_priority);
+            textViewDescription=itemView.findViewById(R.id.text_view_description);
+        }
+    }
+
+}
